dc65199eb26efcda1163793026d3cce1b80776c5
LOX-hu <82037672+LOX-hu@users.noreply.github.com>
2021-04-20 20:33:00 +0700

    3v

--- ping-pong.py
+++ ping-pong.py
@@ -17,38 +17,21 @@ class Player(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 0: self.rect.y -= self.speed
         if keys_pressed[K_s] and self.rect.y < 400: self.rect.y += self.speed
-        # if keys_pressed[K_SPACE]: player.fire()
+        
     def update_left(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 0: self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < 400: self.rect.y += self.speed
-        # if keys_pressed[K_SPACE]: player.fire()    
+           
  
 killed = 0
 lost = 0
 lives = 3
-# class Emeny(GameSprite):
-#     def update(self):
-#         global lost
-#         self.rect.y += self.speed 
-#         if self.rect.y > 500:
-#             self.rect.y = 0
-#             self.rect.x = randint(0,650)
-#             lost += 1
-
-# class Bullet(GameSprite):
-#     def update(self):
-#         self.rect.y -= self.speed 
-#         if self.rect.y < 0:
-#             self.kill()
-
-# mixer.init()
-# mixer.music.load('space.ogg')
-# mixer.music.play()
+ 
 
 clock = time.Clock()
 FPS = 60
-
+ 
  
 
 window = display.set_mode((700, 500))
@@ -56,18 +39,23 @@ display.set_caption("ping-pong")
 background = transform.scale(image.load("galaxy.jpg"), (700, 500))
 player = Player('racket.png',10, 100, 75, 100, 15)
 player2 = Player('racket.png',620, 100, 75, 100, 15)
+ball = GameSprite('tenis_ball.png', 350 , 250, 40, 40, 2)
 
-  
+speed_y = 3
+speed_x = 3  
  
 
 font.init()
 font1 = font.SysFont('Arial', 36)
-
+p1lose_text = font1.render('проиграл левый', 1, (255,255,255))
+p2lose_text = font1.render('проиграл правый', 1, (255,255,255))
 game = True
 finish = False           
 while game:
     for e in event.get():
         if e.type == QUIT: game = False
+
+     
          
                    
 
@@ -77,14 +65,21 @@ while game:
         player.update_right()
         player2.reset()
         player2.update_left()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.x <= 0 or ball.rect.x >= 700:
+            finish = True 
+            window.blit(p1lose_text, (250,400))
+        if ball.rect.y <= 0 :
+            speed_y *= -1     
+        if ball.rect.y > 460 :
+            speed_y *= -1
+
+        if sprite.collide_rect(ball, player2):
+            speed_x *= -1
+        if sprite.collide_rect(ball, player):
+            speed_x *= -1
         clock.tick(FPS)
         
-        # lost_text = font1.render(f'Пропущено: {lost}', 1, (255,255,255))
-        # kill_text = font1.render(f'Убито: {killed}', 1, (255,255,255))
-        # reload_text = font1.render('ПЕРЕЗАРЯДКА', 1, (255,255,255))
-        # window.blit(lost_text,(0,0))
-        # window.blit(kill_text,(0,30))
-
-    
-
     display.update()
